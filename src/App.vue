2ada0fbb041a400281a738aa5f51e283bf736d88
yaredghebre <yared2391@gmail.com>
2023-06-13 12:13:51 -0300

    Milestone 4 and 5 complete

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,40 @@
+<script>
+import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
+
+export default {
+  data() {
+    return {
+      baseUrl: "http://127.0.0.1:8000",
+      projects: [],
+    };
+  },
+  mounted() {
+    this.getProjects();
+  },
+  methods: {
+    getProjects() {
+      axios.get(`${this.baseUrl}/api/projects`).then(resp => {
+        this.projects = resp.data.results;
+      });
+    }
+  },
+  components: { ProjectCard, ProjectCard }
+}
+
+</script>
 
 <template>
-  <h1>Test</h1>
+  <div class="container my-3">
+    <h1>Lista dei progetti</h1>
+    <div class="row row-cols-6 g-3">
+      <div class="col" v-for="project in projects" :key="project.id">
+        <ProjectCard :project="project"/>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: 'ProjectCard',
+  props: {
+    project: Object
+  },
+  data() {
+    return {
+      baseUrl: "http://127.0.0.1:8000",
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="card">
+    <img class="card-img-top" v-if="project.image" :src="`${baseUrl}/storage/${project.image}`" :alt="project.title">
+    <div v-else>
+      <span>Image not available</span>
+    </div>
+    <div class="card-body">
+      <h5>{{ project.title }}</h5>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
