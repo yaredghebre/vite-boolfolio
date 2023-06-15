31671ff7b9c9a57a4c43e18b9bd7d4d907754ed3
yaredghebre <yared2391@gmail.com>
2023-06-15 11:23:57 -0300

    Add routes / ProjectsPage implemented

---
+++ src/pages/GalleryPage.vue
@@ -0,0 +1,19 @@
+<script>
+import axios from 'axios';
+import { store } from '../store';
+import ProjectCard from '../components/ProjectCard.vue';
+import Pagination from '../components/Pagination.vue';
+
+export default {
+    name: 'GalleryPage',
+}
+</script>
+
+<template>
+    <div class="container my-3">
+        <h1>Questa è la Gallery</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+</style>
